b137d63bda80c8bf574ca80ae05c9999940e9289
najmulmyself <najmul.myself@gmail.com>
2024-01-04 17:37:34 +0600

    dummy

--- lib/cubit/todos_cubit.dart
+++ lib/cubit/todos_cubit.dart
@@ -4,18 +4,21 @@ part 'todos_state.dart';
 
 class TodosCubit extends Cubit<TodosInitial> {
   TodosCubit() : super(TodosInitial([]));
-
+ // add task
   addTask(String todo) {
     final todoList = state.todos;
     todoList.add(todo);
     emit(TodosInitial(todoList));
   }
 
+// delete
   deleteTask(int index) {
     final todoList = state.todos;
     todoList.removeAt(index);
     emit(TodosInitial(todoList));
   }
+
+  // edit
 
   editTask(String todo, int index) {
     final todoList = state.todos;
